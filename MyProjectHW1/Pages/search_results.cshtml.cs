3d275dd7a7f643287e1cc5a0c28292007d31187d
Carter Proctor <Carter Proctor@DESKTOP-36G4MDK>
2024-02-02 15:12:53 -0500

    Add project files.

---
+++ MyProjectHW1/Pages/search_results.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyProjectHW1.Pages
+{
+    public class search_resultsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
